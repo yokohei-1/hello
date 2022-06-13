f2e7ae07b13a1e04bbfa906d1c8f71e5cd43d0da
yokohei-1 <60385247+yokohei-1@users.noreply.github.com>
2022-06-13 11:54:26 +0900

    数当てゲーム

---
+++ kazuate.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
+ 
+public class InputSample {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i;
+        System.out.println("数当てゲームです。2桁の数字を入力してください");
+        Random rand = new Random();
+        int num = rand.nextInt(100);
+        for (i = 0;i < 5;i++) {
+            int num1 = scan.nextInt();
+            if (num1 == num) {
+                 System.out.println("あたり!!");
+                 break;
+            } else {
+                System.out.println("ハズレ!!");
+                if (Math.abs(num1-num) > 19) {
+                    System.out.println("正解と20以上の差があります");
+                }
+                if (i == 4) {
+                    System.out.println("ゲームオーバーです。正解は" + num + "です");
+                }
+            }
+        }
+
+        scan.close();
+    }
+}
