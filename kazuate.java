c518d0d11d155eb59ebdc0480ddd1dec69bdef2b
yokohei-1 <60385247+yokohei-1@users.noreply.github.com>
2022-06-15 11:30:33 +0900

    修正5回目

--- kazuate.java
+++ kazuate.java
@@ -8,27 +8,31 @@ public class kazuate {
         System.out.println("数当てゲームです。2桁の数字を入力してください");
         Random rand = new Random();
         int num = rand.nextInt(90) + 10;
-        for (i = 0; i < 5; i++) {
-            int num1 = scan.nextInt();
-            if (num1 == num) {
-                System.out.println("あたり!!");
-                break;
-            } else {
-                System.out.println("ハズレ!!");
-                if (num1 - num > 0) {
-                    System.out.println("正解よりも大きいです");
+        try {
+            for (i = 0; i < 5; i++) {
+                int num1 = scan.nextInt();
+                if (num1 == num) {
+                    System.out.println("あたり!!");
+                    scan.close();
+                    break;
                 } else {
-                    System.out.println("正解よりも小さいです");
-                }
-                if (Math.abs(num1 - num) > 19) {
-                    System.out.println("正解と20以上の差があります");
-                }
-                if (i == 4) {
-                    System.out.println("ゲームオーバーです。正解は" + num + "です");
+                    System.out.println("ハズレ!!");
+                    if (num1 - num > 0) {
+                        System.out.println("正解よりも大きいです");
+                    } else {
+                        System.out.println("正解よりも小さいです");
+                    }
+                    if (Math.abs(num1 - num) > 19) {
+                        System.out.println("正解と20以上の差があります");
+                    }
+                    if (i == 4) {
+                        System.out.println("ゲームオーバーです。正解は" + num + "です");
+                        scan.close();
+                    }
                 }
             }
+        } catch (Exception e) {
+            System.out.println("入力が正しくありません。");
         }
-
-        scan.close();
     }
 }
